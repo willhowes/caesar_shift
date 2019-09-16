7a8919102ca1c532a4db8f92daa8aff9d6a10f37
Will Howes <williameldenhowes@gmail.com>
2019-09-16 15:25:31 +0100

    Refactored all three tests passing

--- lib/caesar_shift.rb
+++ lib/caesar_shift.rb
@@ -2,17 +2,12 @@ ALPHABET = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
               "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 def moving_shift(s, shift)
-
-  unless s == 'a' || s == 'ab'
-    result = s.split('').map do |letter|
-      index = ALPHABET.find_index(letter)
-      letter = ALPHABET[index + shift]
+    result = []
+    s.split('').each_with_index do |letter, index|
+      alpha_index = ALPHABET.find_index(letter)
+      letter = ALPHABET[alpha_index + shift + index]
+      result << letter
+      alpha_index += 1
     end
     return result
-  end
-  if s.length == 1
-    ['b']
-  else
-    ['b','d']
-  end
 end
