31e7b41177a5085616a12398679b7b74d44c9ba1
Will Howes <williameldenhowes@gmail.com>
2019-09-18 16:40:17 +0100

    moving_shift method works for arrays of 7

--- lib/caesar_shift.rb
+++ lib/caesar_shift.rb
@@ -3,20 +3,28 @@ ALPHABET = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
 
 def moving_shift(s, shift)
     result = [[],[],[],[],[]]
+    remainder = s.length % 5
     s.split('').each_with_index do |letter, index|
       alpha_index = ALPHABET.find_index(letter)
       letter = ALPHABET[alpha_index + shift + index]
-      if s.length > 5
-        if index == 1 || index == 0
+      if remainder == 0 || s.length <= 5
+        result[index] << letter
+      elsif remainder == 1 && s.length > 5
+        if index == 0 || index == 1
         result[0] << letter
         else
           result[index -1] << letter
         end
-      else
-        result[index] << letter
+      elsif remainder == 2 && s.length > 5
+        if index == 1 || index == 0
+        result[0] << letter
+        elsif index == 2 || index == 3
+          result[1] << letter
+        else
+          result[index -2] << letter
+        end
       end
       alpha_index += 1
     end
-    result.reject!(&:empty?)
     return result.map {|string| string.join('')}
 end
--- spec/caesar_shift_spec.rb
+++ spec/caesar_shift_spec.rb
@@ -2,18 +2,22 @@ require 'caesar_shift'
 
 describe '#moving_shift' do
   it 'increments a given letter by the number given as a parameter' do
-    expect(moving_shift('a', 1)).to eq(['b'])
+    expect(moving_shift('a', 1)).to eq(['b', '', '', '', ''])
   end
   it 'increments a given letters by the number and plus one for additional letters' do
-    expect(moving_shift('ab',1)).to eq(['b','d'])
+    expect(moving_shift('ab',1)).to eq(['b','d', '', '', ''])
   end
 
   it 'increments by numbers other than one' do
-    expect(moving_shift('c', 2)).to eq(['e'])
+    expect(moving_shift('c', 2)).to eq(['e', '', '', '', ''])
   end
 
   it 'handles string with more than five characters' do
     expect(moving_shift('abcdef', 1)).to eq(['bd','f', 'h', 'j', 'l'])
+  end
+
+  it 'handles a string of more than six character' do
+    expect(moving_shift('abcdefg', 1)).to eq(['bd','fh', 'j', 'l', 'n'])
   end
 end
 
